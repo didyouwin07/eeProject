47febe9fb8c0db1f34cb97e53f2114d21103dfb8
didyouwin07 <vishwasbordia@outlook.com>
2022-05-21 16:45:09 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [isSafe, setIsSafe] = useState(false)
+  const [warningText, setWarningText] = useState('SAFE')
+
+  useEffect(()=>{
+    if(!isSafe){
+      setWarningText('Fault')
+    }
+  },[isSafe])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+      <div className='mainContainer' style={isSafe?{background: 'pink'}:{background: 'red'}}>
+        <div className='heading'>Detection of faults in Power Systems</div>
+        <div className={'warningWrapper'}>
+          {<div className={isSafe ? '': 'blink'}> {warningText} </div>}
+        </div>
+      </div>
+      
+    
   );
 }
 
